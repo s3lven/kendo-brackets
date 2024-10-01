e3f7cae482e4981a86f748999e85086fae349ac7
Eriz <erizjsub2@gmail.com>
2024-10-01 15:57:32 -0700

    build: containerized both front and backend
    notes for future me:
    docker-compose -f docker-compose.dev.yaml up -d --build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,7 +35,7 @@ app.use(
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/", authRouter)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
